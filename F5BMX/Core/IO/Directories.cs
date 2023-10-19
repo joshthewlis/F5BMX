e3b6bef3353911dd6002c577a2f4c2bd588c4914
Josh Thewlis <josh@iancrt.co.uk>
2023-10-19 22:14:21 +0100

    Replace Serialization with IO

---
+++ F5BMX/Core/IO/Directories.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+
+namespace F5BMX.Core.IO;
+
+internal static class Directories
+{
+
+    private static string seriesDirectory { get; set; }
+
+    public static string baseDirectory { get => $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}/F5BMX/{seriesDirectory}"; }
+
+    public static void SetSeries(int year, string name)
+    {
+        seriesDirectory = $"{year}-{name.Replace(" ", "_")}";
+
+        if (Directory.Exists(baseDirectory) == false)
+            Directory.CreateDirectory(baseDirectory);
+    }
+
+    public static List<string> LoadSeries()
+    {
+        if (Directory.Exists(baseDirectory) == false)
+            Directory.CreateDirectory(baseDirectory);
+
+        List<string> series = new List<string>();
+
+        foreach (var item in Directory.EnumerateDirectories(baseDirectory))
+        {
+            series.Add(item.Replace(baseDirectory, ""));
+        }
+
+        return series;
+    }
+
+}
---
+++ F5BMX/Core/IO/JSON.cs
@@ -0,0 +1,21 @@
+﻿using F5BMX.Core.IO;
+using System.IO;
+using System.Text.Json;
+
+namespace F5BMX.Core.Serialization;
+
+static class JSON
+{
+
+    public static T? ReadFile<T>(string fileName) where T : class
+    {
+        return JsonSerializer.Deserialize<T>(File.ReadAllText($"{Directories.baseDirectory}\\{fileName}.json"));
+    }
+
+    public static void WriteFile<T>(string fileName, T obj) where T : class
+    {
+        var json = JsonSerializer.Serialize<T>(obj);
+        File.WriteAllText($"{Directories.baseDirectory}\\{fileName}.json", json);
+    }
+
+}
