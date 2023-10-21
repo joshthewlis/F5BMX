fafbcb7b704f0b4f10741f17288c13966294fa17
Josh Thewlis <josh@iancrt.co.uk>
2023-10-21 10:28:10 +0100

    JSON Write Indented

--- F5BMX/Core/IO/JSON.cs
+++ F5BMX/Core/IO/JSON.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text.Json;
 
 namespace F5BMX.Core.IO;
@@ -6,14 +7,24 @@ namespace F5BMX.Core.IO;
 static class JSON
 {
 
-    public static T? ReadFile<T>(string fileName) where T : class
+    public static T ReadFile<T>(string fileName) where T : class
     {
-        return JsonSerializer.Deserialize<T>(File.ReadAllText($"{Directories.baseDirectory}\\{fileName}.json"));
+        var file = $"{Directories.baseDirectory}\\{fileName}.json";
+
+        if (File.Exists(file) == false)
+            return Activator.CreateInstance<T>();
+
+        var json = JsonSerializer.Deserialize<T>(File.ReadAllText(file));
+
+        if (json == null)
+            return Activator.CreateInstance<T>();
+
+        return json;
     }
 
     public static void WriteFile<T>(string fileName, T obj) where T : class
     {
-        var json = JsonSerializer.Serialize<T>(obj);
+        var json = JsonSerializer.Serialize<T>(obj, new JsonSerializerOptions() { WriteIndented = true });
         File.WriteAllText($"{Directories.baseDirectory}\\{fileName}.json", json);
     }
 
