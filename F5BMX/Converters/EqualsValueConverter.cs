6e1aed9f20ae76a0f142e705dc41f89dba43f86b
Josh Thewlis <josh@iancrt.co.uk>
2023-10-21 22:00:45 +0100

    Round Settings RadioButton isChecked Converter

---
+++ F5BMX/Converters/EqualsValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace F5BMX.Converters
+{
+    internal class EqualsValueConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString() == parameter.ToString())
+                return true;
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return int.Parse(parameter.ToString());
+        }
+
+    }
+}
